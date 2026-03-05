cf49ffbfa1fa3d6dc2a61a1a9861ef7198a0a141
amaal2005 <amaalferas333@gmail.com>
2026-03-05 15:05:43 +0200

    Initial commit for new project

---
+++ src/main/java/com/wasel/wasel/WaselApplication.java
@@ -0,0 +1,13 @@
+package com.wasel.wasel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaselApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaselApplication.class, args);
+    }
+
+}
